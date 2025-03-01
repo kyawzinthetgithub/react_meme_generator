ea0e5340785cbe7d0ebfb56ce09ef33cb125f7ba
Kyaw Zin Thet <kyawzinthetpersonal@gmail.com>
2025-03-02 01:25:57 +0630

    react useEffect (side effect) with dependencies array

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
-import Header from "./components/Header";
-import Main from "./components/Main";
+// import Header from "./components/Header";
+// import Main from "./components/Main";
+
+import { useEffect, useState } from "react";
 
 export default function App() {
+  const [starWarsData, setStarWarsData] = useState(null);
+  const [count, setCount] = useState(1);
+
+  useEffect(() => {
+    console.log('rendered');
+    fetch(`https://swapi.dev/api/people/${count}`)
+      .then((res) => res.json())
+      .then((data) => setStarWarsData(data))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, [count]);
+
   return (
-    <>
-      <Header />
-      <Main />
-    </>
+    <div>
+      <p>count is {count}</p>
+      <button onClick={() => setCount((old) => old + 1)}>Add</button>
+      {starWarsData ? (
+        <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
   );
 }
