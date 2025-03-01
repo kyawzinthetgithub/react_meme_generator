9a51134978f568a0c06ac970d30c56c4415194d1
Kyaw Zin Thet <kyawzinthetpersonal@gmail.com>
2025-03-01 23:51:30 +0630

    set state with handle input

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,27 +1,54 @@
 import ImageSvg from "./ImageSvg";
+import { useState } from "react";
 
 export default function Main() {
+  const [meme, setMeme] = useState({
+    topText: "One does not simply",
+    bottomText: "Walk into Mordor",
+    imageUrl: "http://i.imgflip.com/1bij.jpg",
+  });
+
+  const handleChange = (event) => {
+    const { value, name } = event.currentTarget;
+    setMeme((oldMeme) => ({
+      ...oldMeme,
+      [name]: value,
+    }));
+  };
+
   return (
     <main>
       <div className="form">
         <label>
           Top Text
-          <input type="text" placeholder="One does not simply" name="topText" />
+          <input
+            type="text"
+            placeholder="One does not simply"
+            name="topText"
+            value={meme.topText}
+            onChange={(e) => handleChange(e)}
+          />
         </label>
 
         <label>
           Bottom Text
-          <input type="text" placeholder="Walk into Mordor" name="bottomText" />
+          <input
+            type="text"
+            placeholder="Walk into Mordor"
+            name="bottomText"
+            value={meme.bottomText}
+            onChange={(e) => handleChange(e)}
+          />
         </label>
         <button className="getImageBtn">
           Get a new meme image
-          <ImageSvg />
+          <ImageSvg color={'white'} />
         </button>
       </div>
       <div className="meme">
-        <img src="http://i.imgflip.com/1bij.jpg" />
-        <span className="top">One does not simply</span>
-        <span className="bottom">Walk into Mordor</span>
+        <img src={meme.imageUrl} />
+        <span className="top">{meme.topText}</span>
+        <span className="bottom">{meme.bottomText}</span>
       </div>
     </main>
   );
